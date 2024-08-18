d4181b8820cb9e83576e102e3dd27abdbb34403b
raybull0119 <ray20100119@gmail.com>
2024-08-18 19:20:05 +0800

    新增: [AC]完整的解法

--- basic/d562/input_handle.py
+++ basic/d562/input_handle.py
@@ -2,7 +2,16 @@ while True:
     try:
         a=int(input())
         b=input().split()
-        print(a)
-        print(b)
-    except Exception:
+        # b = [99,77,66,44,11]
+        for i in  range(0, a):
+            # print(b) # b = [99,77,66,44,11]
+            print(' '.join(b))
+            b.reverse()
+            # print(numbers)
+            b.pop()
+            # b = [11,44,66,77]
+        print('')
+        # print(a)
+        # print(b)
+    except EOFError:
         break
